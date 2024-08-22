33ee26fa44a96bd572819c0ec805ca47739db136
dev1 <dev1@gmail.com>
2024-08-22 18:25:31 +0530

    FirstCommit1

--- src/main/java/Dev.java
+++ src/main/java/Dev.java
@@ -2,6 +2,7 @@ public class Dev {
     public static void main(String[] args) {
         System.out.println("dev");
         System.out.println("master");
+        somelogic
         void test("master");
          void test("bracnh1");
         void test2("bracnh1");
